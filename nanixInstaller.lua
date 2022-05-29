65dedf36c14347c0b5f5676daf5274f603c611dc
igorkll <69525332+igorkll@users.noreply.github.com>
2022-05-29 11:49:27 +0300

    Add files via upload

--- nanixInstaller.lua
+++ nanixInstaller.lua
@@ -9,6 +9,7 @@ if command == "open" then
     os.execute("/usr/bin/nanix.lua")
 elseif command == "uninstall" then
     fs.remove("/usr/bin/nanix.lua")
+    fs.remove(su.getPath())
 elseif command == "install" then
     assert(su.saveFile("/usr/bin/nanix.lua", su.getInternetFile("https://raw.githubusercontent.com/igorkll/appMarket3/main/nanix.lua")))
 end
