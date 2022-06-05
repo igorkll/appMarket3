21bb48159c76818a06429cf66a20d57087939ad8
igorkll <69525332+igorkll@users.noreply.github.com>
2022-06-05 15:26:52 +0300

    Add files via upload

--- LibDeflateInstaller.lua
+++ LibDeflateInstaller.lua
@@ -6,10 +6,9 @@ local fs = require("filesystem")
 local command = ...
 
 if command == "open" then
-    os.execute("/usr/bin/LibDeflate.lua")
 elseif command == "uninstall" then
-    fs.remove("/usr/bin/LibDeflate.lua")
+    fs.remove("/usr/lib/LibDeflate.lua")
     fs.remove(su.getPath())
 elseif command == "install" then
-    assert(su.saveFile("/usr/bin/LibDeflate.lua", assert(su.getInternetFile("https://raw.githubusercontent.com/igorkll/appMarket3/main/LibDeflate.lua"))))
+    assert(su.saveFile("/usr/lib/LibDeflate.lua", assert(su.getInternetFile("https://raw.githubusercontent.com/igorkll/appMarket3/main/LibDeflate.lua"))))
 end
