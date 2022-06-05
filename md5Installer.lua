dd46724e7b138b68d6ac8fa61cacdf485ab6834e
igorkll <69525332+igorkll@users.noreply.github.com>
2022-06-05 15:27:34 +0300

    Add files via upload

--- md5Installer.lua
+++ md5Installer.lua
@@ -6,10 +6,9 @@ local fs = require("filesystem")
 local command = ...
 
 if command == "open" then
-    os.execute("/usr/bin/md5.lua")
 elseif command == "uninstall" then
-    fs.remove("/usr/bin/md5.lua")
+    fs.remove("/usr/lib/md5.lua")
     fs.remove(su.getPath())
 elseif command == "install" then
-    assert(su.saveFile("/usr/bin/md5.lua", assert(su.getInternetFile("https://raw.githubusercontent.com/igorkll/appMarket3/main/md5.lua"))))
+    assert(su.saveFile("/usr/lib/md5.lua", assert(su.getInternetFile("https://raw.githubusercontent.com/igorkll/appMarket3/main/md5.lua"))))
 end
